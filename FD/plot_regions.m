5983486337f4de80be06a7b7d6f80af8e33f905e
John Alexander <paulalexander23@live.co.uk>
2017-10-24 23:24:14 +0100

    no comment

--- FD/plot_regions.m
+++ FD/plot_regions.m
@@ -1,16 +1,16 @@
-function plot_regions()
+function plot_regions(h, t, x, m2, m3)
     %plot_regions Calculates the different regions of stability/class and plots
     %them
     %   WIP
     
-    k = 0.1;
+    %k = 0.1;
     type = zeros(length(x), length(t));
     
     for t_i = 1:length(t)
         for x_i = 1:length(x)
             %type(x_i,t_i) = max(real(DispersionRelation(k, h(x_i,1,t_i), h(x_i,2,t_i), m2, m3, s1, s2, Q)));
             %type(x_i,t_i) = eigDR(k, h(x_i,1,t_i), h(x_i,2,t_i), m2, m3, s1, s2, Q);
-            type(x_i,t_i) = max(imag(eig(G_nonlinear(h(t_i,x_i), h(t_i,x_i+end/2), m2, m3))))>0;
+            type(x_i,t_i) = max(imag(eig(compute_g_nonlinear(h(t_i,x_i), h(t_i,x_i+end/2), m2, m3))))>0;
         end
     end
     
