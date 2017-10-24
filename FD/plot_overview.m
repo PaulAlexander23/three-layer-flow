4a75ba6c48b7ce08fe53882a21fb73ba6c3a9c97
John Alexander <paulalexander23@live.co.uk>
2017-10-24 23:55:24 +0100

    -

---
+++ FD/plot_overview.m
@@ -0,0 +1,23 @@
+function plot_overview(h,t,x,H1,H2)
+    %PLOT_OVERVIEW Calls several plot functions to get an overview of the
+    %solution
+    
+    plot_interfaces(x,h(1,:),H1,H2)
+    
+    figure
+    plot_interfaces(x,h(end,:),H1,H2)
+    
+    figure
+    plot_surfaces(h,t,x,H1,H2)
+    
+    figure
+    plot_l1_norm(h,t,x)
+    
+    figure
+    plot_log_l2_norm(h,t,x)
+    
+    figure
+    plot_regions(h,t,x,m2,m3)
+    
+end
+
