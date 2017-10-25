1d7463af17955734c4486c67e714e759147880b5
paul <paulalexander23@live.co.uk>
2017-10-25 12:00:22 +0100

    -

--- FD/test_method.m
+++ FD/test_method.m
@@ -1,9 +1,13 @@
 % Driver for the numerical methods
 
 %clear all
-
-%addpath('data','data/parameters','plots');
-load('data/parameters/ic-unstable-h-0.46-0.54-mu-0.3-0.5-s-1-1-Q-1.mat','-mat');
+Q = 2;
+H1 = 0.5;
+H2 = 0.6;
+m2 = 0.5;
+m3 = 1.4;
+s1 = 10;
+s2 = 10;
 
 t_final = 5;
 t_count = 2^3;
@@ -14,13 +18,15 @@ x_count = 2^5;
 x_step = x_length/x_count;
 x = linspace(x_step, x_length, x_count)';
 
-func = @(t,y) f_full_problem(y, Q, s1, s2, m2, m3);
+func = @(t,y) f_evolution(y, Q, H1, H2, m2, m3, s1, s2);
 inter = i_double_cos(x, H1, H2,0.01,pi/2,x_length);
 
 
 % Set Up Finite Differences
-InitialiseFiniteDifferences(length(x),x(2)-x(1),4)
+initialise_finite_differences(length(x),x(2)-x(1),4)
 
+
+%%
 tic
 [t, h] = ode2b(func, t, inter);
 toc
