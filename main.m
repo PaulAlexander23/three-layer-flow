17e7aa03ec1b8707f8dbce397e84d8c2ad071d5b
paul <paulalexander23@live.co.uk>
2017-10-25 11:56:01 +0100

    -

--- main.m
+++ main.m
@@ -1,13 +1,13 @@
 % Driver for the grouping of everything
 
-clear;
+%clear;
 close;
 
 addpath('DR','FD','NS')
 
 Q = 2;
-H1 = 0.4;
-H2 = 0.7;
+H1 = 0.5;
+H2 = 0.6;
 m2 = 0.5;
 m3 = 1.4;
 s1 = 10;
@@ -17,11 +17,12 @@ s2 = 10;
 
 plot_dispersion_relation_overview(H1,H2,m2,m3,s1,s2,Q,1);
 
+
 %%
 
 t_final = 100;
 x_length = 2*pi;
-x_count = 2^8;
+x_count = 2^6;
 x_step = x_length/x_count;
 x = linspace(x_step, x_length, x_count)';
 
@@ -31,7 +32,7 @@ initialise_finite_differences(length(x),x(2)-x(1),4)
 %func = @(t,y) f_evolution_linear(y, Q, H1, H2, m2, m3, s1, s2);
 func = @(t,y) f_evolution(y, Q, H1, H2, m2, m3, s1, s2);
 
-inter = i_double_cos(x, 0, 0, 0.05, pi/2, x_length);
+inter = i_double_cos(x, 0, 0, 0.01, pi/2, x_length);
 %inter = i_double_rand(x, 0, 0,0.2);
 
 figure
