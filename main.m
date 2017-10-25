508d2b6e5385fa3c4bd327561d5394ad71d0b16a
paul <paulalexander23@live.co.uk>
2017-10-25 11:08:28 +0100

    -

--- main.m
+++ main.m
@@ -1,16 +1,17 @@
 % Driver for the grouping of everything
 
 clear;
+close;
 
-addpath('DR\','FD\','NS\')
+addpath('DR','FD','NS')
 
 Q = 2;
 H1 = 0.4;
 H2 = 0.7;
 m2 = 0.5;
 m3 = 1.4;
-s1 = 10;
-s2 = 0.2;
+s1 = 0.1;
+s2 = 0.1;
 
 %%
 
@@ -18,19 +19,20 @@ plot_dispersion_relation_overview(H1,H2,m2,m3,s1,s2,Q,1);
 
 %%
 
-t_final = 1;
+t_final = 10;
 x_length = 2*pi;
-x_count = 2^6;
+x_count = 2^8;
 x_step = x_length/x_count;
 x = linspace(x_step, x_length, x_count)';
 
 % Set Up Finite Differences
 initialise_finite_differences(length(x),x(2)-x(1),4)
 
-func = @(t,y) f_evo(y, Q, H1, H2, m2, m3, s1, s2);
+%func = @(t,y) f_linear(y, Q, H1, H2, m2, m3, s1, s2);
+func = @(t,y) f_evolution(y, Q, H1, H2, m2, m3, s1, s2);
 
-%inter = i_double_cos(x, 0, 0,0.1,0,x_length);
-inter = i_double_rand(x, 0, 0,0.2);
+inter = i_double_cos(x, 0, 0, 0.05, 0, x_length);
+%inter = i_double_rand(x, 0, 0,0.2);
 
 figure
 plot_steady_state(H1,H2,m2,m3,Q)
