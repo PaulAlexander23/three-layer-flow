3513facf26b2e704e2ed76d6c27face2f6fb90f2
paul <paulalexander23@live.co.uk>
2017-10-27 14:08:16 +0100

    -

--- FD/script_primary_sounding.m
+++ FD/script_primary_sounding.m
@@ -1,8 +1,10 @@
-% Driver for the numerical methods
-% Need to remove C
+%SCRIPT_PRIMARY_SOUNDING 
+
 
 clear;
 
+tFinal = 300;
+xCount = 2^8;
 H1 = 0.4;
 H2 = 0.7;
 
@@ -16,13 +18,8 @@ for i = 2:5
     C(i,:) = kron(repmat(values,1,(l)^(i-2)),ones(1,l^(5-i)));
 end
 
-inter = i_double_rand(x, 0, 0, 0.1);
 
-plot(x,inter(1:end/2),x,inter(end/2+1:end))
-
-pause
-%%
-for n = 1:size(C,2)
+for n = 2:size(C,2)
     
     Q = C(1,n);
     s1 = C(2,n);
@@ -30,9 +27,9 @@ for n = 1:size(C,2)
     m2 = C(4,n);
     m3 = C(5,n);
     
-    [h,x,t]=compute_numerical_solution(H1,H2,m2,m3,s1,s2 ,Q,tFinal,xCount,@(x) i_double_rand(x,0.1));
+    [h,x,t]=compute_numerical_solution(H1,H2,m2,m3,s1,s2 ,Q,tFinal,xCount,@(x) i_double_rand_fixed(x,0.1));
     
-    filename = sprintf(sprintf('data/output/evening16october/data-x-%g-t-%g-h-%g-%g-mu-%g-%g-s-%g-%g-Q-%g.mat',x_length,t_final,H1,H2,m2,m3,s1,s2,Q));
+    filename = sprintf(sprintf('../data/sounding/data-x-%g-t-%g-h-%g-%g-mu-%g-%g-s-%g-%g-Q-%g.mat',xCount,tFinal,H1,H2,m2,m3,s1,s2,Q));
     save(filename);
     
 end
