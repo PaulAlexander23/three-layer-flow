2963f3d1184e689a5e7ae447f7c579b9ed195a79
paul <paulalexander23@live.co.uk>
2017-10-31 11:46:10 +0000

    -

--- main.m
+++ main.m
@@ -6,12 +6,14 @@ close all;
 addpath('DR','FD','NS')
 
 Q = 1.0;
-H1 = 0.45;
-H2 = 0.65;
-m2 = 0.7;
-m3 = 0.45;
-s1 = 0.5;
-s2 = 2.0;
+H1 = 0.5;
+H2 = 0.7;
+m2 = 0.5;
+m3 = 0.25375;
+s1 = 1;
+s2 = 1;
+
+inter = @(x) i_double_cos(x,0.001,0.1);
 
 %%
 omega = compute_dispersion_relation(1,H1,H2,m2,m3,s1,s2,Q)';
@@ -19,13 +21,21 @@ fprintf('Dispersion Relation: w+(1) = %f%+fi, w-(1) = %f%+fi\n',omega(1),omega(1
 
 plot_dispersion_relation_overview(H1,H2,m2,m3,s1,s2,Q,2);
 
-pause
+figure();
+
+plot_interface_type(inter,Q,H1,H2,m2,m3);
+
+commandwindow()
+
+pause;
+
+close all;
 %%
 tFinal = 200;
 xCount = 2^5;
 
-[hLinear,x,tLinear]=compute_numerical_linear_solution(H1,H2,m2,m3,s1,s2 ,Q,tFinal,xCount,@(x) i_double_cos(x,0.02,0));
-[h,~,t]=compute_numerical_solution(H1,H2,m2,m3,s1,s2 ,Q,tFinal,xCount,@(x) i_double_cos(x,0.02,0));
+[hLinear,x,tLinear]=compute_numerical_linear_solution(H1,H2,m2,m3,s1,s2 ,Q,tFinal,xCount,inter);
+[h,~,t]=compute_numerical_solution(H1,H2,m2,m3,s1,s2 ,Q,tFinal,xCount,inter);
 
 
 %%
