5b1426786095347d815d5697eb19e1e6bc1a1844
paul <paulalexander23@live.co.uk>
2017-10-31 11:24:32 +0000

    -

--- NS/main.m
+++ NS/main.m
@@ -1,22 +1,21 @@
 
 
-Q = 1.3;
-H1 = 0.4;
-H2 = 0.5;
-m2 = 0.75;
-m3 = 1.3;
+H1 = 0.5;
+H2 = 0.7;
+m2 = 0.5;
+m3 = 0.25;
+Q = 1;
 
-figure
+figure('position',[0,660,560,420])
 plot_single_case(Q,H1,H2,m2,m3)
 
-figure
+figure('position',[660,660,1120,420])
 plot_varying_perturbations(Q,H1,H2,m2,m3)
 
-figure
+figure('position',[0,0,560,420])
 plot_varying_theta(H1,H2,m2,m3,Q)
 
-figure
+figure('position',[660,0,560,420])
 plot_varying_theta_interfaces(H1,H2)
 
-
-
+commandwindow();
