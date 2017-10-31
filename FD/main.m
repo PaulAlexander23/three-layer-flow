41fb3e48a3cd89c1e79509f3fd755c1119b0e1f6
paul <paulalexander23@live.co.uk>
2017-10-31 11:46:10 +0000

    -

--- FD/main.m
+++ FD/main.m
@@ -14,10 +14,10 @@ tFinal = 200;
 xCount = 2^6;
 
 %i_custom = @(x, a, theta) [a*cos(x+theta),zeros(size(x))];
-i_custom = @(x, a, theta) [zeros(size(x)),-a*cos(x+theta)];
+%i_custom = @(x, a, theta) [zeros(size(x)),-a*cos(x+theta)];
 
 [hLinear,~,tLinear]=compute_numerical_linear_solution(H1,H2,m2,m3,s1,s2 ,Q,tFinal,xCount,@(x) i_custom(x,0.01,0));
-[h,x,t]=compute_numerical_solution(H1,H2,m2,m3,s1,s2 ,Q,tFinal,xCount,@(x) i_custom(x,0.001,0));
+[h,x,t]=compute_numerical_solution(H1,H2,m2,m3,s1,s2 ,Q,tFinal,xCount,@(x) i_double_cos(x,0.001,0.1));
 
 figure
 plot_overview(hLinear,tLinear,x,Q,H1,H2,m2,m3)
