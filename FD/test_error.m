062e74595a378170973e4df4b47df16dc397cf38
John Alexander <paulalexander23@live.co.uk>
2017-11-02 16:15:49 +0000

    -

--- FD/test_error.m
+++ FD/test_error.m
@@ -11,7 +11,7 @@ s1 = 1;
 s2 = 1;
 
 tFinal = 10;
-inter = @(x) i_double_cos(x, 0.05, pi/2);
+inter = @(x) i_double_cos(x, 0.1, pi/2);
 
 xCount = 2.^(4:9);
 xN = length(xCount);
@@ -31,7 +31,6 @@ for i = 1:xN
 end
 
 %%
-
 for i = 1:xN-1
     error(i) = norm(h{i}(:,end)-h{xN}(1:xCount(xN)/xCount(i):end,end))*sqrt(2*pi/xCount(i));
     fprintf('Method %u, error: %g, Time taken: %f,\n',i,error(i),timeTaken(i))
--- FD/test_method_2.m
+++ FD/test_method_2.m
@@ -20,7 +20,7 @@ x = linspace(x_step, x_length, x_count)';
 initialise_finite_differences(length(x),x(2)-x(1),4)
 
 func = @(t,y) f_evolution(y, Q, H1, H2, m2, m3, s1, s2);
-inter = @(x) i_double_cos(x, 0.05, pi/2);
+inter = @(x) i_double_cos(x, 0.1, pi/2);
 
 noMethods = 3;
 timeTaken = ones(noMethods,1);
@@ -28,20 +28,20 @@ error = ones(noMethods,1);
 h = cell(noMethods,1);
 t = cell(noMethods,1);
 
-%options = odeset('Event',@(t,y) event_collision(t,y,H1,H2));
-
+options = odeset('Vectorized','on',...
+                 'Event',@(t,y) event_collision(t,y,H1,H2));
 tic;
-[t{1}, h{1}] = ode15s(func, [0, t_final], inter(x));
+[t{1}, h{1}] = ode15s(func, [0, t_final], inter(x), options);
 timeTaken(1) = toc;
 h{1} = h{1}';
 
 tic;
-[t{2}, h{2}] = ode23t(func, [0, t_final], inter(x));
+[t{2}, h{2}] = ode23t(func, [0, t_final], inter(x), options);
 timeTaken(2) = toc;
 h{2} = h{2}';
 
 tic;
-[t{3}, h{3}] = ode23tb(func, [0, t_final], inter(x));
+[t{3}, h{3}] = ode23tb(func, [0, t_final], inter(x), options);
 timeTaken(3) = toc;
 h{3} = h{3}';
 
@@ -53,18 +53,19 @@ x2 = linspace(x2_step, x2_length, x2_count)';
 
 initialise_finite_differences(length(x2),x2(2)-x2(1),4)
 
-options = odeset('RelTol',1e-10,'AbsTol',1e-12);
+options = odeset('Vectorized','on',...
+                 'Event',@(t,y) event_collision(t,y,H1,H2));
+
 tic;
 [t_approx,h_approx] = ode15s(func,[0,t_final],inter(x2),options);
 timeTaken_approx = toc;
 h_approx = h_approx';
 
 %%
-
 for i = 1:noMethods
     error(i) = norm(h{i}(:,end)-h_approx(1:(x2_count/x_count):end,end))...
                 *sqrt(x_step);
     fprintf('Method %u, error: %g, Time taken: %f,\n',i,error(i),timeTaken(i))
 end
-
+%%
 save('test_method_2_results2.mat')
