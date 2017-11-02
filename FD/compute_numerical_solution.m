7d69a08d4973408c3f3087fd689c32c781949bcd
John Alexander <paulalexander23@live.co.uk>
2017-11-02 13:43:29 +0000

    Vectorized f_evolution.m

--- FD/compute_numerical_solution.m
+++ FD/compute_numerical_solution.m
@@ -12,11 +12,11 @@ function [h,x,t] = compute_numerical_solution(H1, H2, m2, m3, s1, s2, Q, tFinal,
     func = @(t,y) f_evolution(y, Q, H1, H2, m2, m3, s1, s2);
     
     %options = odeset('RelTol',1e-8,'AbsTol',1e-10);
-    options = odeset('Event',@(t,y) event_collision(t,y,H1,H2));
-    %options = odeset('Vectorized','on');
+    %options = odeset('Event',@(t,y) event_collision(t,y,H1,H2));
+    options = odeset('Vectorized','on','Event',@(t,y) event_collision(t,y,H1,H2));
     
     tic
-    [t, h, te, ye, ie] = ode15s(func, [0,tFinal], inter(x), options);
+    [t, h, te, ye, ie] = ode23tb(func, [0,tFinal], inter(x), options);
     toc
     
     if ~isempty(te)
