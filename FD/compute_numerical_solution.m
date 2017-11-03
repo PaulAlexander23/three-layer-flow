3d84c06c3a027d9bd5c672f54add7309e1476d4e
paul <paulalexander23@live.co.uk>
2017-11-03 15:04:34 +0000

    -

--- FD/compute_numerical_solution.m
+++ FD/compute_numerical_solution.m
@@ -14,7 +14,7 @@ function [h,x,t] = compute_numerical_solution(H1, H2, m2, m3, s1, s2, Q, tFinal,
     %options = odeset('RelTol',1e-8,'AbsTol',1e-10);
     options = odeset('Vectorized','on',...
                      'Event',@(t,y) event_collision(t,y,H1,H2));
-    
+                 
     tic
     [t, h, te, ye, ie] = ode15s(func, [0,tFinal], inter(x), options);
     toc
