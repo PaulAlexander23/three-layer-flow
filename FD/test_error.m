9269157a0d417ad0f95c347ae52d901d52fca619
paul <paulalexander23@live.co.uk>
2017-11-06 16:39:05 +0000

    -

--- FD/test_error.m
+++ FD/test_error.m
@@ -10,12 +10,13 @@ m3 = 1;
 s1 = 1;
 s2 = 1;
 
-tFinal = 10;
+tFinal = 0.5;
 inter = @(x) i_double_cos(x, 0.1, pi/2);
 
 % xCount = 2.^(4:8);
-xCount = 25:25:150;
+xCount = 25:25:250;
 xN = length(xCount);
+dx = 2*pi./xCount;
 
 h = cell(xN,1);
 x = cell(xN,1);
@@ -33,7 +34,8 @@ end
 
 %%
 for i = 1:xN-1
-    error(i) = norm(h{i}(:,end)-h{xN}(1:xCount(xN)/xCount(i):end,end))*sqrt(2*pi/xCount(i));
+    % error(i) = norm(h{i}(:,end)-h{xN}(1:xCount(xN)/xCount(i):end,end))*sqrt(dx(i));
+    error(i) = norm(h{i}(:,end)-h{xN}(1:xCount(xN)/xCount(i):end,end));%*dx(i);
     fprintf('Method %u, error: %g, Time taken: %f,\n',i,error(i),timeTaken(i))
 end
 fprintf('Method %u, error: -, Time taken: %f,\n',i+1,timeTaken(xN));
@@ -54,3 +56,5 @@ b2 = X\log(error);
 scatter(log(xCount(1:end-1)),log(error));
 hold on
 plot(log(xCount(1:end-1)),X*b2);
+
+b2(2)
