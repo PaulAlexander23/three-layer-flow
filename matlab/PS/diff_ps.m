c9b45235b0499f1221ca4b96a94bfd7b0e4b6d5c
Paul Alexander <paulalexander23@live.co.uk>
2017-11-15 09:47:42 +0000

    -

--- matlab/PS/diff_ps.m
+++ matlab/PS/diff_ps.m
@@ -6,11 +6,15 @@ function dy = diff_ps(y, degree, suppression)
     end
     
     yF = fft(y);
+    
     N = length(yF)/2;
-    k = fftshift([0,-N+1:N-1])';
+    
+    %k = fftshift([0,-N+1:N-1])';
+    k = [0:N-1, 0, 1-N:-1]';
+    
     dyF = (1i*k).^degree.*yF;
     
-    dyF(dyF < suppression*N*2)= 0 ;
+    dyF(dyF < suppression*N*2) = 0 ;
     
     dy = real(ifft(dyF));
     
