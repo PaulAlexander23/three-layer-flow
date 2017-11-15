285cd201b17177aa96d4d07d012d554b05a6ac08
Paul Alexander <paulalexander23@live.co.uk>
2017-11-15 13:30:19 +0000

    Sorted folders: IF, DR, NS.

--- matlab/IF/f_evolution.m
+++
@@ -1,5 +0,0 @@
-function F = f_evolution(y, Q, H1, H2, m2, m3, s1, s2)
-
-    yCell = {y(1:end/2,:), y(end/2+1:end,:)
-
-end
--- matlab/NS/main.m
+++ matlab/NS/main.m
@@ -7,13 +7,13 @@ addpath('../IF/','../FD/','../PS/')
 Q = 1;
 H1 = 0.4;
 H2 = 0.7;
-m2 = 1;
+m2 = 0;
 m3 = 1;
 s1 = 1;
 s2 = 1;
 
 tFinal = 1;
-xCount = 2^5;
+xCount = 2^6;
 
 %i_custom = @(x, a, theta) [a*cos(x+theta),zeros(size(x))];
 %i_custom = @(x, a, theta) [zeros(size(x)),-a*cos(x+theta)];
@@ -21,7 +21,7 @@ xCount = 2^5;
 %[hLinear,~,tLinear] = compute_numerical_linear_solution(H1,H2,m2,m3,...
  %                      s1,s2,Q,tFinal,xCount,@(x) i_double_cos(x,0.001,0));
 [h,x,t]=compute_numerical_solution(H1,H2,m2,m3,s1,s2 ,Q,tFinal,xCount,...
-                                           @(x) i_double_rand_fixed(x,0.1));
+                                   @(x) i_double_rand_fixed(x,0.1));
 
 %%
 %%figure
