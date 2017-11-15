2c371bf10afba199944cae5460f2b90efd006d86
Paul Alexander <paulalexander23@live.co.uk>
2017-11-15 12:51:35 +0000

    -

--- matlab/IF/f_thin_film.m
+++ matlab/IF/f_thin_film.m
@@ -1,5 +1,5 @@
-function [ F ] = f_thin_film( y, c )
-global D1 D3 D4
-F = c * (y.^3 .* (D4 * y) + y.^2 .* (D1 * y) .* (D3 * y) );
-end
+function [ F ] = compute_thin_film( y, dy, c )
 
+    F = c * (y{1}.^3 .* (dy{4}) + y{1}.^2 .* (dy{1}) .* (dy{3}) );
+
+end
