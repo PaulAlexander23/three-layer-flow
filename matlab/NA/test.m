5aa4e69be1c1df4600534d4acef6a9e7deeb2c4e
Paul Alexander <paulalexander23@live.co.uk>
2017-11-16 22:18:11 +0000

    -

--- matlab/NA/test.m
+++ matlab/NA/test.m
@@ -1,3 +1,4 @@
+% Testing and examples for the nonlinear analysis section
 
 addpath('../IF')
 
@@ -8,17 +9,28 @@ m3 = 0.65;
 Q = 1;
 
 
-
 compute_g_nonlinear(0.3,0.5,H1,H2,m2,m3,Q)
 
 %%
 
-figure('position',[0,660,560,420])
+figure
+plot_interface_type(@(x) i_double_cos(x,0.1,0),H1,H2,m2,m3,Q);
+
+figure
 plot_type_for_all_h(H1,H2,m2,m3,Q)
 
-figure('position',[660,660,560,420])
+x = linspace(0,2*pi)';
+t = linspace(0,10);
+h = i_double_cos(x,0.1,pi/4)*exp(-0.5*t);
+
+figure
+plot_type_regions(h,t,x,H1,H2,m2,m3,Q)
+
+figure
 plot_varying_perturbations(@i_double_cos,H1,H2,m2,m3,Q)
 
-figure('position',[1320,660,560,420])
+figure
 plot_varying_theta(@i_double_cos,H1,H2,m2,m3,Q)
 
+figure
+plot_varying_theta_interfaces(H1,H2)
