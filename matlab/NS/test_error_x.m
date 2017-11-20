19e229aa4bbbe4576c12801d1c8b5b1d3647e4d6
Paul Alexander <paulalexander23@live.co.uk>
2017-11-20 13:47:33 +0000

    -

--- matlab/NS/test_error_x.m
+++ matlab/NS/test_error_x.m
@@ -1,4 +1,5 @@
-%Error testing
+%Error testing - IS THIS NEEDED IN THE X DIRECTION? WHAT TOLERANCE ARE WE
+%USING?
 
 addpath('../IF/')
 
@@ -13,8 +14,8 @@ s2 = 1;
 tFinal = 1;
 inter = @(x) i_double_cos(x, 0.1, pi/2);
 
-xCount = 2.^(2:7);
-%xCount = 2^5:2^4:2^7;
+xCount = 2.^(4:8);
+%xCount = 2^5:2^5:2^8;
 %xCount = 25:25:250;
 xN = length(xCount);
 dx = 2*pi./xCount;
@@ -25,28 +26,24 @@ t = cell(xN,1);
 
 
 timeTaken = ones(xN,1);
-error_norm = ones(xN-1,1);
-error_value = ones(xN-1,1);
+error = ones(xN-1,1);
 
 for i = 1:xN
     tic;
     [h{i},x{i},t{i}] = compute_numerical_solution(H1,H2,m2,m3,s1,s2,Q,tFinal,xCount(i),inter);
     timeTaken(i) = toc;
 end
-
 %%
+
 for i = 1:xN-1
-    error_norm(i) = norm(h{i}(:,end)-h{xN}(xCount(xN)/xCount(i):xCount(xN)/xCount(i):end,end))*sqrt(dx(i));
-    fprintf('Method %u, error: %g, Time taken: %f,\n',i,error_norm(i),timeTaken(i))
+    error(i) = max(abs(h{i}(:,end) - h{xN}(xCount(xN)/xCount(i):xCount(xN)/xCount(i):end,end)));
+    fprintf('Method %u, error: %g, Time taken: %f,\n',i,error(i),timeTaken(i))
 end
 fprintf('Method %u, error: -, Time taken: %f,\n',i+1,timeTaken(xN));
 
-for i = 1:xN-1
-    error_value(i) = max(abs(h{i}(:,end) - h{xN}(xCount(xN)/xCount(i):xCount(xN)/xCount(i):end,end)));
-end
 
 %%
-%save('test_error_x_results.mat')
+save('test_error_x_results.mat')
 
 %%
 % hold on
@@ -56,12 +53,16 @@ end
 
 %%
 plot_loglog_with_regression(xCount,timeTaken);
+title({'A log - log plot of the computation time',' against the number of x steps'})
+xlabel('x steps, 10^x')
+ylabel('Computation time, 10^y')
+
 
 figure
-plot_loglog_with_regression(xCount(1:end-1),error_value);
-
-figure
-plot_loglog_with_regression(xCount(1:end-1),error_norm);
+plot_loglog_with_regression(xCount(1:end-1),error);
+title({'A log - log plot of the error',' against the number of x steps'})
+xlabel('x steps, 10^x')
+ylabel('Error, 10^y')
 
 function gradient = plot_loglog_with_regression(x,y)
     X = [ones(length(x),1) log10(x)'];
