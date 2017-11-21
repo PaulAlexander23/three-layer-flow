cbc131ac9c36ce197e2316e392ad9f72718cd53d
Paul Alexander <paulalexander23@live.co.uk>
2017-11-21 14:20:49 +0000

    -

--- matlab/NS/compute_numerical_solution.m
+++ matlab/NS/compute_numerical_solution.m
@@ -1,8 +1,18 @@
-function [h,x,t] = compute_numerical_solution(H1, H2, m2, m3, s1, s2, Q, tFinal, xN, inter)
+function [h,x,t] = compute_numerical_solution(H1, H2, m2, m3, s1, s2, Q, inter, tFinal, xL, xN, RelTol)
     %COMPUTE_NUMERICAL_SOLUTION Computes the numerical solution up to tFinal
     %   Detailed explanation goes here
     
-    xL = 2*pi;
+    if nargin < 10
+        xL = 2*pi;
+    end
+    if nargin < 11
+        xN = 2^8;
+    end
+    if nargin < 12
+        RelTol = 1e-3;
+    end
+    
+    
     xS = xL/xN;
     x = linspace(xS, xL, xN)';
     
@@ -17,7 +27,7 @@ function [h,x,t] = compute_numerical_solution(H1, H2, m2, m3, s1, s2, Q, tFinal,
     
     options = odeset('Vectorized','on',...
         'Event',@(t,y) event_collision(t,y,H1,H2),...
-        'RelTol',1e-3,... % Default: 1e-3
+        'RelTol',RelTol,... % Default: 1e-3
         'AbsTol',1e-6);  % Default: 1e-6
     
     tic
