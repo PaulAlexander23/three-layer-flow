f7fe8268fc3e877ba8750a3f9ec2f55a1c5d291a
Paul Alexander <paulalexander23@live.co.uk>
2018-01-29 17:00:42 +0000

    Started work on determining long term behaviour. Need an equation to solve for c.
    Former-commit-id: 4c4617e450a6184249577ef44e2d1a6905fc283b

---
+++ matlab/LT/main.m
@@ -0,0 +1,26 @@
+
+H1 = 0.5;
+H2 = 0.95;
+m2 = 0.06;
+m3 = 0.015;
+s1 = 1;
+s2 = 1;
+Q = 1;
+
+
+xL = 2*pi;
+xN = 128;
+xS = xL/xN;
+
+x = linspace(xS, xL, xN)';
+
+y0 = [1;i_double_rand_fixed(x, 0.01)];
+
+func = @(y) [1;...
+    y(1) * y(2:end) + rhs_ps(0, x, y(2:end), ...
+    @(t, x, y, dy) compute_evolution(y, dy, H1, H2, m2, m3, s1, s2, Q),...
+    [1,3,4])];
+
+func(y0)
+
+%fsolve(func,y0);
