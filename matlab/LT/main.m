c49c404346ab70741361df4228c8bfc06f4a3f9f
Paul Alexander <paulalexander23@live.co.uk>
2018-02-05 16:52:22 +0000

    Added a fixed point to ensure a unique solution and used the predicted linear speed as the wave velocity. Not working due to rhs_ps producing high growth rates for the high order modes.
    Former-commit-id: 94cf3bd74e05e534159437cbcfd8d89aff710fa5

--- matlab/LT/main.m
+++ matlab/LT/main.m
@@ -14,13 +14,14 @@ xS = xL/xN;
 
 x = linspace(xS, xL, xN)';
 
-y0 = [1;i_double_rand_fixed(x, 0.01)];
+%y0 = [1;i_double_rand_fixed(x, 0.01)];
+%y0 = [-1.242; h(:,end)];
+c = imag(compute_dispersion_relation(1,H1,H2,m2,m3,s1,s2,Q));
+y0 = [c(1); h(:,end)];
 
-func = @(y) [1;...
-    y(1) * y(2:end) + rhs_ps(0, x, y(2:end), ...
+func = @(y) [y(2)-y0(2);...
+    y(1) * diff_ps(y(2:end),1) + rhs_ps(0, x, y(2:end), ...
     @(t, x, y, dy) compute_evolution(y, dy, H1, H2, m2, m3, s1, s2, Q),...
     [1,3,4])];
 
-func(y0)
-
-%fsolve(func,y0);
+y = fsolve(func,y0);
