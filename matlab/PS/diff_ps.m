5f26032f19dbce372d08145da408590917519a78
Paul <paulalexander23@live.co.uk>
2018-02-16 08:52:15 +0000

    Reset changes to diff_ps and rhs_ps. Now they don't set the zero's mode of eta to zero before the calculation of the rhs.
    Former-commit-id: 14214ea2729134ebc8093aba84d19eef648788ea

--- matlab/PS/diff_ps.m
+++ matlab/PS/diff_ps.m
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 function dy = diff_ps(y, degree, suppression)
     %DIFF_PSEUDO_SPECTRAL Uses the pseudo-spectral method to differentiate
     %   Detailed explanation goes here
@@ -28,48 +27,4 @@ function dy = diff_ps(y, degree, suppression)
     % Transform back into real space
     dy = ifft(dyF);
     
-=======
-function [dy, y] = diff_ps(y, diffOrders, suppression)
-    %DIFF_PSEUDO_SPECTRAL Uses the pseudo-spectral method to differentiate
-    %   Detailed explanation goes here
-    if nargin < 3
-       suppression = 1e-13;
-    end
-    
-    L = size(y,1);
-    
-    arrayF = zeros(size(y,1),size(y,2),1+length(diffOrders));
-    
-    % Transform into fourier space
-    arrayF(:,:,1) = fft(y);
-    
-    N = L/2;
-    
-    % Determine k in matlab form
-    %k = fftshift([0,-N+1:N-1])';
-    k = [0:N-1, 0, 1-N:-1]';
-    
-    % Remove zeroth mode
-    %arrayF(1,:,1) = 0;
-    %arrayF(1 + N,:,1) = 0;
-    
-    % Prior suppression
-    arrayF(abs(arrayF)<suppression) = 0;
-    
-    % Apply pseudo-spectral differentiation
-    for j = 1:length(diffOrders)
-        arrayF(:,:,j+1) = (1i*k).^diffOrders(j).*arrayF(:,:,1);
-    end
-    
-    % Posterior suppression
-    % arrayF(abs(arrayF) < suppression*N*2) = 0 ;
-    % arrayF(abs(arrayF) < suppression*max(abs(arrayF))) = 0 ;
-    
-    %Transform back into real space
-    array = ifft(arrayF);
-    
-    y = array(:,:,1);
-    dy = array(:,:,2:end);
-    
->>>>>>> Stashed changes
 end
--- matlab/PS/rhs_ps.m
+++ matlab/PS/rhs_ps.m
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 function F = rhs_ps(t, x, y, func, diffOrders)
     
     nOI = size(y,1)/size(x,1); % Number of interfaces
@@ -17,41 +16,11 @@ function F = rhs_ps(t, x, y, func, diffOrders)
     
     F = func(t, x, yCell, dyCell);
     
-    %     % Remove changes in mode 0 as this is conserved
-%     FF = fft(F);
-%     N = size(FF,1)/2;
-%     k = [0:N-1, 0, 1-N:-1]';
-%     FF(k==0,:) = 0;
-%     F = ifft(FF);
-
-=======
-function F = rhs_ps(t, x, y, func, diffOrders)
-    
-    nOI = size(y,1)/size(x,1); % Number of interfaces
-    
-    yCell = cell(nOI,1);
-    dyCell = cell(max(diffOrders),nOI);
-    
-    for j = 1:nOI
-        [dy,ny] = diff_ps(y(1 + end*(j-1)/nOI:end*j/nOI,:),diffOrders);
-        
-        yCell{j} = ny;
-        for k = 1:length(diffOrders)
-            dyCell{diffOrders(k),j} = dy(:,:,1+j);
-        end
-    end
-    
-    F = func(t, x, yCell, dyCell);
-    
     % Remove changes in mode 0 as this is conserved
-    N = size(F,1)/4;
-    FF = fft(reshape(F,2*N,2*size(F,2)));
-    FF(1,:) = 0;
-    FF(1+N,:) = 0;
-    F = reshape(ifft(FF),4*N,size(F,2));
-%     k = [0:N-1, 0, 1-N:-1]';
-%     FF(k==0,:) = 0;
-%     F = ifft(FF);
-    
->>>>>>> Stashed changes
+    %     FF = fft(F);
+    %     N = size(FF,1)/2;
+    %     k = [0:N-1, 0, 1-N:-1]';
+    %     FF(k==0,:) = 0;
+    %     F = ifft(FF);
+
 end
