4382fad649995f394efa6b68bd9dd7bfe98c6192
Paul Alexander <paulalexander23@live.co.uk>
2018-03-16 13:01:42 +0000

    Pruned files and wrote up a save and load script.
    Former-commit-id: 03e4e4ca28234ede4ec98a218265403e1f9b427d

--- matlab/NS/untitled3.m
+++
@@ -1,6 +0,0 @@
-
-H1 = 0.4; H2 = 0.7; m2 = 0.5; m3 = 1; s1 = 0; s2 = 0; Q = 1;
-tL = 10;
-
-[y,x,t] = compute_numerical_solution_fourier(H1,H2,m2,m3,s1,s2,Q,...
-    @(x) i_double_cos(x,0.01,1),tL,2*pi,2^6,1e-3);
