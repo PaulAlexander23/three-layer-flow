e706ba99769e4c80f5dbc23b3280c75a2a2ef136
Paul <paulalexander23@live.co.uk>
2018-03-19 09:51:31 +0000

    commit in order to merge
    Former-commit-id: 23b300e066c433e1b3006f59d0ababc640f1f74e

--- matlab/LA/plot_eigenfunctions.m
+++ matlab/LA/plot_eigenfunctions.m
@@ -4,12 +4,23 @@ function plot_eigenfunctions(a, k, H1, H2, m2, m3, s1, s2, Q)
     
     y = compute_eigenfunctions(x,a,k,H1,H2,m2,m3,s1,s2,Q);
     
-    plot(x,H1 + y(1:end/2),x,H2 + y(1+end/2:end));
+    figure;
+    subplot(2,1,1);
+    plot(x,y(1,1:end/2));
     
-    title('An Eigenfunction')
-    xlabel('x')
-    ylabel('y')
+    title('An Eigenfunction');
     
-    axis([0,2*pi,0,1])
+    %xlabel('x');
+    ylabel('\eta_1');
+    xlim([0,2*pi]);
+    axis('equal')
+    
+    subplot(2,1,2);
+    plot(x,y(1,1+end/2:end));
+    
+    xlabel('x');
+    ylabel('\eta_2');
+    xlim([0,2*pi]);
+    axis('equal')
     
 end
