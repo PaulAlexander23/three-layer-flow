4a0148fdb2374b54a787d9a6b835e5c469cf3eda
Paul <paulalexander23@live.co.uk>
2018-03-20 00:53:10 +0000

    Changed eigenfunction plotting
    Former-commit-id: 5f90a7016582eda3753c10bdb4763a1b52ff714c

--- matlab/LA/compute_eigenfunctions.m
+++ matlab/LA/compute_eigenfunctions.m
@@ -1,7 +1,6 @@
 function [y, lambda] = compute_eigenfunctions( x, a, mode,...
         H1, H2, m2, m3, s1, s2, Q)
-    %COMPUTE_EIGENFUNCTIONS Returns the sum of the eigenfunctions given
-    %scalings of them
+    %COMPUTE_EIGENFUNCTIONS Returns the eigenfunctions and eigenvalues.
     %   x - Column vector, Function evaluation points
     %   a - Column vector, Scalings of the eigenfunctions [1;0] for the most
     %   unstable and [0;1] for the most stable
@@ -9,8 +8,7 @@ function [y, lambda] = compute_eigenfunctions( x, a, mode,...
     
     
     [lambda, V] = compute_dispersion_relation(mode,H1,H2,m2,m3,s1,s2,Q);
-    
-    y = [real(exp(1i*mode*x) * [1,V(1,2)/V(1,1)] * a )',...
-            real(exp(1i*mode*x) * [1,V(2,2)/V(2,1)] * a )'];
 
+    y = a * real(kron([1,1;V(1,2)/V(1,1),V(2,2)/V(2,1)],exp(1i*mode*x)));
+    
 end
--- matlab/LA/plot_eigenfunctions.m
+++ matlab/LA/plot_eigenfunctions.m
@@ -6,21 +6,40 @@ function plot_eigenfunctions(a, k, H1, H2, m2, m3, s1, s2, Q)
     
     figure;
     subplot(2,1,1);
-    plot(x,y(1,1:end/2));
+    plot(x,y(1+end/2:end,1));
     
-    title('An Eigenfunction');
+    title('Least stable Eigenfunction');
     
     %xlabel('x');
-    ylabel('\eta_1');
-    xlim([0,2*pi]);
-    axis('equal')
-    
-    subplot(2,1,2);
-    plot(x,y(1,1+end/2:end));
-    
-    xlabel('x');
     ylabel('\eta_2');
     xlim([0,2*pi]);
     axis('equal')
     
+    subplot(2,1,2);
+    plot(x,y(1:end/2,1));
+    
+    xlabel('x');
+    ylabel('\eta_1');
+    xlim([0,2*pi]);
+    axis('equal')
+   
+    figure;
+    subplot(2,1,1);
+    plot(x,y(1+end/2:end,2));
+    
+    title('Most stable Eigenfunction');
+    
+    %xlabel('x');
+    ylabel('\eta_2');
+    xlim([0,2*pi]);
+    axis('equal')
+
+        
+    subplot(2,1,2);
+    plot(x,y(1:end/2,2));
+    
+    xlabel('x');
+    ylabel('\eta_1');
+    xlim([0,2*pi]);
+    axis('equal')
 end
