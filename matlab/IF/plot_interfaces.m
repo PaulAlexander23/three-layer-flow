8c9a5838e52c7887e4b8a98dcaee7ab532a014d0
Paul Alexander <paulalexander23@live.co.uk>
2018-04-20 15:00:23 +0100

    Separate interface amplitudes.
    Former-commit-id: 638dab968afbfb976aa241fc6750284874628b0c

--- matlab/IF/plot_interfaces.m
+++ matlab/IF/plot_interfaces.m
@@ -1,9 +1,9 @@
 function plot_interfaces(x, y, H1, H2)
     %plot_interfaces Plot the interfaces.
     
-    plot(x, H1 + y(1:end/2), x, H2 + y(end/2+1:end))
+    plot(x, H1 + y(1:end/2), 'k', x, H2 + y(end/2+1:end),'k')
     xlabel('x')
     ylabel('y')
-    title('A plot of the interfaces.')
+    %title('A plot of the interfaces.')
     axis([0,2*pi,0,1])
 end
