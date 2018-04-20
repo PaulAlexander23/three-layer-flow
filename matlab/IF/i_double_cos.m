c547a8082846a8e6fce3f8cac45f2b642e02817c
Paul Alexander <paulalexander23@live.co.uk>
2018-04-20 15:03:00 +0100

    Separate interface amplitudes.
    Former-commit-id: 5949b364054cb1ad13d4fc61f747a113d5962c20

--- matlab/IF/i_double_cos.m
+++ matlab/IF/i_double_cos.m
@@ -1,4 +1,4 @@
 function h = i_double_cos(x, a, theta)
-    h = a * [cos(x); cos(x+theta)];
-    %h = [a(1)*cos(x); a(2)*cos(x+theta)];
+    %h = a * [cos(x); cos(x+theta)];
+    h = [a(1)*cos(x); a(2)*cos(x+theta)];
 end
