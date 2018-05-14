c49a313d76306830a3a0ec0ac2221b43a6a4eb3f
Paul Alexander <paulalexander23@live.co.uk>
2018-05-14 11:41:59 +0100

    Added plotting part to the testing error script. Both Rel and Abs error.
    Former-commit-id: 12fc633837f5b2f70bd6279e56656420c7c6fb4e

--- matlab/test/test_error_x.m
+++ matlab/test/test_error_x.m
@@ -1,22 +1,17 @@
-%Error testing - IS THIS NEEDED IN THE X DIRECTION? WHAT TOLERANCE ARE WE
-%USING?
+%Error testing - Resolution Increase
 
-addpath('../interfaces/','../compute/','../plots/')
-
-Q = 1;
-H1 = 0.4;
-H2 = 0.7;
+H1 = 1/3;
+H2 = 2/3;
 m2 = 1;
 m3 = 1;
 s1 = 1;
 s2 = 1;
+Q = 1;
 
-tFinal = 1;
-inter = @(x) i_double_cos(x, 0.1, pi/2);
+tFinal = 1000;
+inter = @(x) i_double_cos(x, [0.1,0.1], pi/2);
 
-xCount = 2.^(4:8);
-%xCount = 2^5:2^5:2^8;
-%xCount = 25:25:250;
+xCount = 2.^(4:10);
 xN = length(xCount);
 dx = 2*pi./xCount;
 
@@ -24,53 +19,42 @@ h = cell(xN,1);
 x = cell(xN,1);
 t = cell(xN,1);
 
-
 timeTaken = ones(xN,1);
-error = ones(xN-1,1);
 
 for i = 1:xN
     tic;
-    [h{i},x{i},t{i}] = compute_numerical_solution(H1,H2,m2,m3,s1,s2,Q,tFinal,xCount(i),inter);
+    [h{i},x{i},t{i}] = compute_numerical_solution(H1,H2,m2,m3,s1,s2,Q,inter,tFinal,2*pi,xCount(i));
     timeTaken(i) = toc;
 end
-%%
 
-for i = 1:xN-1
-    error(i) = max(abs(h{i}(:,end) - h{xN}(xCount(xN)/xCount(i):xCount(xN)/xCount(i):end,end)));
-    fprintf('Method %u, error: %g, Time taken: %f,\n',i,error(i),timeTaken(i))
+%save('test_error_x_results.mat')
+
+%%
+hBest = 6;
+tNew = linspace(0,t{hBest}(end),200);
+
+hI = cell(7,1);
+for i = 1:7
+    hI{i} = compute_interpolation(h{i},t{i},x{i},tNew,x{hBest});
 end
-fprintf('Method %u, error: -, Time taken: %f,\n',i+1,timeTaken(xN));
-
-
-%%
-save('test_error_x_results.mat')
-
-%%
-% hold on
-% for i = 1:xN
-% plot(x{i}, h{i}(1:end/2,end))
-% end
+hError = cell(7,1);
+for i = 1:7
+    hError{i} = hI{i} - hI{hBest};
+end
 
 %%
-plot_loglog_with_regression(xCount,timeTaken);
-title({'A log - log plot of the computation time',' against the number of x steps'})
-xlabel('x steps, 10^x')
-ylabel('Computation time, 10^y')
+for i = 1:7
+    plot_log_l2_norm(hError{i},tNew,x{hBest});
+end
 
+err = zeros(7,1);
+for i = 1:7
+    %err(i) =
+    %norm(hError{i}(:,end))*(x{hBest}(2)-x{hBest}(1))*(2*pi/xCount(hBest));
+    %Abs
+    err(i) = norm(hError{i}(:,end))/norm(h{hBest}(:,end)); %Rel
+end
 
 figure
-plot_loglog_with_regression(xCount(1:end-1),error);
-title({'A log - log plot of the error',' against the number of x steps'})
-xlabel('x steps, 10^x')
-ylabel('Error, 10^y')
+scatter_with_lobf(log2(xCount),log10(err));
 
-function gradient = plot_loglog_with_regression(x,y)
-    X = [ones(length(x),1) log10(x)'];
-    c = X\log10(y);
-    scatter(log10(x),log10(y));
-    hold on
-    plot(log10(x),X*c);
-    hold off
-
-    gradient = c(2);
-end
