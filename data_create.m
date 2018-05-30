5bf76c269e7ce3458ead4783078c3e4b46725ccd
Paul Alexander <paulalexander23@live.co.uk>
2018-05-30 15:53:08 +0100

    Correct data_save and at inter argument to data_create.m.

--- data_create.m
+++ data_create.m
@@ -1,13 +1,9 @@
-function data_create(H1, H2, m2, m3, s1, s2, Q, tL, xL, xN)
-
-    addpath('../interfaces/','../compute/')
-
-    inter = @(x) i_double_rand_fixed(x,0.01);
+function data_create(H1, H2, m2, m3, s1, s2, Q, inter, tL, xL, xN)
 
     RelTol = 1e-3; % Default: 1e-3
 
     tic;
-    [h,t,x] = compute_numerical_solution(H1, H2, m2, m3, s1, s2, Q, inter, tL, xL, xN, RelTol);
+    [h,x,t] = compute_numerical_solution(H1, H2, m2, m3, s1, s2, Q, inter, tL, xL, xN, RelTol);
     timeTaken = toc;
 
     data_save(h,t,x,timeTaken,xN,tL,H1,H2,m2,m3,s1,s2,Q);
