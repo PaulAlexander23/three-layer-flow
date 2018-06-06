8cc38b84667e146996e30e3ecf35a0d46ebaf0e0
Paul Alexander <paulalexander23@live.co.uk>
2018-06-06 16:14:55 +0100

    Correct to the first five modes being unstable.

--- i_double_rand.m
+++ i_double_rand.m
@@ -1,12 +1,12 @@
-function h = i_double_rand(x, a, seed)
-    if nargin > 2
+function h = i_double_rand(x, a, xL, seed)
+    if nargin > 3
         rng(seed);
     end
     
     r = rand(1,10);
     t = rand(1,10)*2.0*pi;
     
-    h = (r.*cos(x*[1:5,1:5] + t))*kron(eye(2),ones(5,1));
+    h = (r.*cos(2*pi/xL*(x*[1:5,1:5] + t)))*kron(eye(2),ones(5,1));
     h = a * h ./ max(max(abs(h)));
     h = [h(:,1);h(:,2)];
 end
