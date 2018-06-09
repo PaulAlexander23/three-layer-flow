58b9ad316ecd3dd36e66d408983e71e88aa36034
Paul Alexander <paulalexander23@live.co.uk>
2018-06-09 10:20:03 +0100

    Correct passed arguments to data save.

--- data_create.m
+++ data_create.m
@@ -6,6 +6,6 @@ function data_create(H1, H2, m2, m3, s1, s2, Q, inter, tL, xL, xN)
     [h,x,t] = compute_numerical_solution(H1, H2, m2, m3, s1, s2, Q, inter, tL, xL, xN, RelTol);
     timeTaken = toc;
 
-    data_save(h,t,x,timeTaken,xN,tL,H1,H2,m2,m3,s1,s2,Q);
+    data_save(h,t,x,timeTaken,xN,xL,H1,H2,m2,m3,s1,s2,Q);
 
 end
