79e94add4781167a81fd4c475441b08d6312eea3
Paul Alexander <paulalexander23@live.co.uk>
2018-06-09 10:40:03 +0100

    Promote xL in data save.

--- data_save.m
+++ data_save.m
@@ -1,6 +1,6 @@
 function data_save(h,t,x,timeTaken,xN,xL,H1,H2,m2,m3,s1,s2,Q)
 
     save(sprintf('data-h-%g-%g-m-%g-%g-s-%g-%g-Q-%g-xN-%g-tF-%g-xL-%g.mat',...
-        H1,H2,m2,m3,s1,s2,Q,xN,ceil(t(end)),ceil(xL)));
+        H1,H2,m2,m3,s1,s2,Q,xN,ceil(xL),ceil(t(end))));
 
 end
