39994d5c637e2982fbc0dabbb3f7d5f770971676
Paul Alexander <paulalexander23@live.co.uk>
2018-06-29 13:47:20 +0100

    Correct data_save.m.

--- data_save.m
+++ data_save.m
@@ -1,6 +1,6 @@
 function data_save(h,t,x,timeTaken,xN,xL,H1,H2,m2,m3,s1,s2,Q)
 
-    save(sprintf('data-h-%g-%g-m-%g-%g-s-%g-%g-Q-%g-xN-%g-tF-%g-xL-%g.mat',...
+    save(sprintf('data-h-%g-%g-m-%g-%g-s-%g-%g-Q-%g-xN-%g-xL-%g-tF-%g.mat',...
         H1,H2,m2,m3,s1,s2,Q,xN,ceil(xL),ceil(t(end))));
 
 end
